8f8e3b811edb7ed790f29e4466d894f763b6a567
Mario Syti <msyti@outlook.com>
2021-06-22 22:38:25 +0300

    Done

---
+++ ActivitiesManager/Controllers/Customers1Controller.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ActivitiesManager.DAL;
+using ActivitiesManager.Models;
+
+namespace ActivitiesManager.Controllers
+{
+    public class Customers1Controller : Controller
+    {
+        private ApplicationDBContext db = new ApplicationDBContext();
+
+        // GET: Customers1
+        public ActionResult Index()
+        {
+            var customers = db.Customers.Include(c => c.CustomerType);
+            return View(customers.ToList());
+        }
+
+        // GET: Customers1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers1/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerTypeID = new SelectList(db.CustomerTypes, "ID", "Description");
+            return View();
+        }
+
+        // POST: Customers1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,CustomerTypeID,Address")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerTypeID = new SelectList(db.CustomerTypes, "ID", "Description", customer.CustomerTypeID);
+            return View(customer);
+        }
+
+        // GET: Customers1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerTypeID = new SelectList(db.CustomerTypes, "ID", "Description", customer.CustomerTypeID);
+            return View(customer);
+        }
+
+        // POST: Customers1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,CustomerTypeID,Address")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerTypeID = new SelectList(db.CustomerTypes, "ID", "Description", customer.CustomerTypeID);
+            return View(customer);
+        }
+
+        // GET: Customers1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ActivitiesManager/Migrations/202106211543256_Annots.cs
@@ -0,0 +1,16 @@
+namespace ActivitiesManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Annots : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
